75b2a6b9858e7bc2326c2de144e71bfa272ee848
Burak Ersoy <DCH6712@driscoll.dch>
2018-04-17 16:06:43 -0500

    EOB -- 04172018

---
+++ 500-WhyWhatISeeIsNeverTheWayItReallyIs.R
@@ -0,0 +1,8 @@
+# The first law of data analysis is garbage in, garbage out!
+# Artefacts vs.Facts
+# Bad measurement processes vs. Biased selection processes
+# Data holes (missing data) vs. Automatic bias
+# Design good data collection processes at the outset
+# Validity vs. Reliability
+# Self-selection bias vs. Low response rates
+# Sampling frames vs Population of interest

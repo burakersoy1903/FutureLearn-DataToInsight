559ce6e649595cda275058024e0851799305f1a6
Burak Ersoy <DCH6712@driscoll.dch>
2018-04-20 10:34:16 -0500

    EOB -- 04202018

--- 500-WhyWhatISeeIsNeverTheWayItReallyIs.R
+++ 500-WhyWhatISeeIsNeverTheWayItReallyIs.R
@@ -6,3 +6,17 @@
 # Validity vs. Reliability
 # Self-selection bias vs. Low response rates
 # Sampling frames vs Population of interest
+# The bar widths are proportional to the group size.
+# https://www.rdocumentation.org/packages/gpairs/versions/1.2/topics/gpairs
+# Random error
+# Population vs. Sample vs. Sampling Distribution
+
+
+# --- 5.13 Exercise - Sampling variation - numeric data ----------
+# No Coding
+# Check "Exercises/5.13-Explore-sampling-variation-numeric_vitonline16.pdf"
+
+
+# --- 5.14 Exercise - Sampling variation - proportions ----------
+# No Coding
+# Check "Exercises/5.14 -Explore-sampling-variation-proportions_vitonline16.pdf"

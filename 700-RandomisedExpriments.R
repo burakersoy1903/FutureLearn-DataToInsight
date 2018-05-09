c294eae6da1b6601e21da72f18e1e2ba396f9f2e
Burak Ersoy <DCH6712@driscoll.dch>
2018-05-09 14:38:42 -0500

    EOB -- 05092018

---
+++ 700-RandomisedExpriments.R
@@ -0,0 +1,16 @@
+# treatments and treatment groups and control group
+# blocking on age
+# randomisation variation
+# randomisation test and re-randomisation
+# luck of the randomisation draw
+# tail proportion -- closer to zero better!
+# large p-value --> no evidence that there are true treatment-group differences
+# small p-value --> does provide evidence that there are true treatment-group differences
+# Furthermore, the smaller the p-value, the stronger the evidence that true treatment differences exist.
+
+
+# --- 7.7 Exercise - Using VIT to run a randomisation test ----------
+# Check "Exercises/7.07-Using-vit-to-run-a-randomisation.pdf"
+
+# --- 7.10 Exercise - Using VIT to run a randomisation test for proportions ----------
+# Check "Exercises/7.10-using-vit-to-run-a-randomisation-test-for-proportions.pdf"
